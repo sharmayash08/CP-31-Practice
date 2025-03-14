0b5fd0553a405018bc4968c7487b8ca17177ec63
Yash Sharma <yash280876@gmail.com>
2025-03-14 23:20:24 +0530

    Done with 1899A

---
+++ 800/1899A.cpp
@@ -0,0 +1,20 @@
+//Game with Integers
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    
+    while(t--){
+        int n;
+        cin >> n;
+        
+        if(n % 3 == 0) cout << "Second" << endl;
+        else cout << "First" << endl;
+    }
+    
+    return 0;
+}
